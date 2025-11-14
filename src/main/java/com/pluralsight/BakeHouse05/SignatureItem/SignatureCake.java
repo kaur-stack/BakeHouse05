d4098c486fb4558d6d32d2d2f8923a42bb1978fa
github <skaur@my.yearupunited.org>
2025-11-14 03:58:52 -0800

    Commiting Signature challenge!

---
+++ src/main/java/com/pluralsight/BakeHouse05/SignatureItem/SignatureCake.java
@@ -0,0 +1,129 @@
+package com.pluralsight.BakeHouse05.SignatureItem;
+
+import com.pluralsight.BakeHouse05.Products.Cake;
+import com.pluralsight.BakeHouse05.Models.CakeSize;
+import com.pluralsight.BakeHouse05.BaseRoom.Topping;
+import com.pluralsight.BakeHouse05.Models.ToppingCategory;
+
+import java.util.List;
+
+public class SignatureCake extends Cake {
+    private final SignatureType signatureType;
+    private boolean isCustomized;
+
+    public SignatureCake(SignatureType signatureType) {
+        // Call parent Cake constructor with default size and base type
+        super(signatureType.getDefaultSize(), signatureType.getBaseType());
+        this.signatureType = signatureType;
+        this.isCustomized = false;
+
+        // Add the signature toppings/fillings for this cake
+        addSignatureToppings();
+    }
+
+    public SignatureCake(SignatureType signatureType, CakeSize customSize) {
+        // Call parent Cake constructor with custom size
+        super(customSize, signatureType.getBaseType());
+        this.signatureType = signatureType;
+        this.isCustomized = false;
+
+        // Add the signature toppings/fillings for this cake
+        addSignatureToppings();
+    }
+
+    // Adds the pre-defined toppings/fillings based on signature type
+    private void addSignatureToppings() {
+        switch (signatureType) {
+            case RED_VELVET_DELIGHT:
+                // Cream Cheese Filling (Premium)
+                super.addTopping(new Topping("Cream Cheese Filling", ToppingCategory.PREMIUM, false));
+                // White Chocolate Covered Strawberries (Premium, treated as topping)
+                super.addTopping(new Topping("White Chocolate Strawberries", ToppingCategory.PREMIUM, false));
+                // Buttercream base (Regular)
+                super.addTopping(new Topping("Buttercream", ToppingCategory.REGULAR, false));
+                break;
+
+            case DUBAI_CHOCOLATE_DREAM:
+                // Pistachio Cream Filling (Premium)
+                super.addTopping(new Topping("Pistachio Cream", ToppingCategory.PREMIUM, false));
+                // Chocolate Ganache Topping (Regular)
+                super.addTopping(new Topping("Chocolate Ganache", ToppingCategory.REGULAR, false));
+                // Dubai Chocolate pieces (Premium)
+                super.addTopping(new Topping("Dubai Chocolate", ToppingCategory.PREMIUM, false));
+                break;
+
+            case TIRAMISU_CLASSIC:
+                // Custard Filling (Regular)
+                super.addTopping(new Topping("Custard", ToppingCategory.REGULAR, false));
+                // Mocha Cream (Regular - for coffee flavor)
+                super.addTopping(new Topping("Mocha Cream", ToppingCategory.REGULAR, false));
+                // Cocoa Powder (represented as topping)
+                super.addTopping(new Topping("Cocoa Powder", ToppingCategory.REGULAR, false));
+                break;
+        }
+    }
+
+    @Override
+    public void addTopping(Topping topping) {
+        super.addTopping(topping);
+        isCustomized = true;
+    }
+
+    public boolean removeTopping(String toppingName) {
+        List<Topping> toppings = getToppings();
+
+        // Find and remove the topping
+        for (int i = 0; i < toppings.size(); i++) {
+            if (toppings.get(i).getName().equalsIgnoreCase(toppingName)) {
+                toppings.remove(i);
+                isCustomized = true;
+                return true;
+            }
+        }
+
+        return false;  // Topping not found
+    }
+
+    private List<Topping> getToppings() {
+        // This assumes we add a getToppings() method to the Cake class
+        // For now, we'll use reflection or provide a workaround
+        return super.getToppingsInternal();
+    }
+
+    @Override
+    public String getDescription() {
+        StringBuilder sb = new StringBuilder();
+
+        // Show signature name prominently
+        sb.append("⭐ ").append(signatureType.getName());
+        if (isCustomized) {
+            sb.append(" (Customized)");
+        }
+        sb.append("\n");
+
+        // Show size and base type
+        sb.append("   ").append(getSize().getDisplay()).append(" | ");
+        sb.append(getType().getDisplay()).append("\n");
+
+        // Show toppings
+        List<Topping> toppings = getToppings();
+        if (!toppings.isEmpty()) {
+            sb.append("  Toppings & Fillings:\n");
+            for (Topping topping : toppings) {
+                sb.append("    - ").append(topping.toString()).append("\n");
+            }
+        }
+
+        // Special message
+        if (hasSpecialMessage()) {
+            sb.append("  Special Message: Yes (+$5.00)\n");
+        }
+
+        sb.append(String.format("  Price: $%.2f", calculatePrice()));
+
+        return sb.toString();
+    }
+
+    public SignatureType getSignatureType() { return signatureType; }
+    public boolean isCustomized() { return isCustomized; }
+}
---
+++ src/main/java/com/pluralsight/BakeHouse05/SignatureItem/SignatureType.java
@@ -0,0 +1,42 @@
+package com.pluralsight.BakeHouse05.SignatureItem;
+
+import com.pluralsight.BakeHouse05.Models.CakeSize;
+import com.pluralsight.BakeHouse05.Models.CakeType;
+
+public enum SignatureType {
+    RED_VELVET_DELIGHT(
+            "Red Velvet Delight",
+            "Red velvet cake with white chocolate covered strawberries and cream cheese filling",
+            CakeSize.MEDIUM,
+            CakeType.RED_VELVET
+    ),
+    DUBAI_CHOCOLATE_DREAM(
+            "Dubai Chocolate Dream",
+            "Chocolate cake with pistachio filling and chocolate ganache topping",
+            CakeSize.MEDIUM,
+            CakeType.CHOCOLATE
+    ),
+    TIRAMISU_CLASSIC(
+            "Tiramisu Classic",
+            "Coffee-infused layers with ladyfingers, custard, and cocoa",
+            CakeSize.MEDIUM,
+            CakeType.VANILLA
+    );
+
+    private final String name;
+    private final String description;
+    private final CakeSize defaultSize;
+    private final CakeType baseType;
+
+    SignatureType(String name, String description, CakeSize defaultSize, CakeType baseType) {
+        this.name = name;
+        this.description = description;
+        this.defaultSize = defaultSize;
+        this.baseType = baseType;
+    }
+
+    public String getName() { return name; }
+    public String getDescription() { return description; }
+    public CakeSize getDefaultSize() { return defaultSize; }
+    public CakeType getBaseType() { return baseType; }
+}
